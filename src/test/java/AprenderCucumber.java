60195dd749e652d874a33b640181611261f47f41
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2020-12-15 22:21:44 -0300

    Reaproveitando cenários de testes, pode ser utilizado REGEX (para versões anteriores)

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -3,6 +3,11 @@ import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import org.junit.Assert;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class AprenderCucumber {
     @Dado("que criei o arquivo corretamente")
     public void que_criei_o_arquivo_corretamente() {
@@ -37,5 +42,42 @@ public class AprenderCucumber {
     @Então("o valor do contador será {int}")
     public void o_valor_do_contador_será(Integer int1) {
         Assert.assertEquals(int1, contador);
+    }
+
+    Date dataEntrega = new Date();
+
+    @Dado("que o pedido foi realizado dia {int}\\/{int}\\/{int}")
+    public void que_o_pedido_foi_realizado_dia(Integer dia, Integer mes, Integer ano) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, dia);
+        cal.set(Calendar.MONTH, mes);
+        cal.set(Calendar.YEAR, ano);
+
+        dataEntrega = cal.getTime();
+    }
+
+    @Quando("a entrega atrasar {int} dias")
+    public void o_sistema_calcular_que_a_entrega_será_realizada_no_dia(Integer dia) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(dataEntrega);
+        cal.add(Calendar.DAY_OF_MONTH, dia);
+
+        dataEntrega = cal.getTime();
+    }
+    @Então("O pedido deve ser entregue na data {int}\\/{int}\\/{int}")
+    public void o_pedido_deve_ser_entregue_na_data(Integer dia, Integer mes, Integer ano) {
+        Date dataRealEntrega = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, dia);
+        cal.set(Calendar.MONTH, mes);
+        cal.set(Calendar.YEAR, ano);
+
+        dataRealEntrega = cal.getTime();
+
+        DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String dataRealEntregaFormatada = sdf.format(dataRealEntrega);
+        String dataEntregaFormatada = sdf.format(dataEntrega);
+
+        Assert.assertEquals(dataRealEntregaFormatada, dataEntregaFormatada);
     }
 }
--- src/test/java/aprender-cucumber.feature
+++ src/test/java/aprender-cucumber.feature
@@ -7,7 +7,17 @@ Cenario: Deve executar especificação
   Quando executá-lo
   Então a especificação deve finalizar com sucesso
 
-Cenário: Deve incrementar contador
+Cenário: Deve incrementar contador com sucesso
   Dado que o contador é 15
   Quando eu adicionar em 3
   Então o valor do contador será 18
+
+Cenário: Deve incrementar contador com sucesso 2
+  Dado que o contador é 20
+  Quando eu adicionar em 10
+  Então o valor do contador será 30
+
+Cenário: Ao realizar um pedido, deve ser entregue no dia que o sistema calculou para entrega
+  Dado que o pedido foi realizado dia 01/12/2020
+  Quando a entrega atrasar 5 dias
+  Então O pedido deve ser entregue na data 06/12/2020
